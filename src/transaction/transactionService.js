d9b388456bb6778988c963a44c34ac6537aabbc3
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2021-06-14 10:30:51 +0700

    add transaction

--- src/transaction/transactionService.js
+++ src/transaction/transactionService.js
@@ -4,16 +4,25 @@ export const createTransaction = async (req, res) => {
   try {
     const newTransaction = new TransactionModel(req.body);
     const saved = await newTransaction.save();
-    console.log(saved);
-    return res.status(200).json(saved.toJSON({ virtuals: true }));
+    const transaction = await TransactionModel.findById(saved._id)
+      .populate('user')
+      .populate('asset')
+      .populate('prev');
+
+    return res.status(200).json(transaction.toJSON({ virtuals: true }));
   } catch (error) {
     return res.status(400).json(error);
   }
 };
 export const getTransaction = async (req, res) => {
   try {
-    const transactions = await TransactionModel.find({}).populate('user').populate('asset').populate('prev');
-    return res.status(200).json(transactions.map((item) => item.toJSON({ virtuals: true })));
+    const transactions = await TransactionModel.find({})
+      .populate('user')
+      .populate('asset')
+      .populate('prev');
+    return res
+      .status(200)
+      .json(transactions.map((item) => item.toJSON({ virtuals: true })));
   } catch (error) {
     return res.status(400).json(error);
   }
