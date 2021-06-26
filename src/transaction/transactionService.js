f79ce9f14faded6bd8513102b8507d6edd342545
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2021-06-26 21:35:15 +0700

    improve create transaction

--- src/transaction/transactionService.js
+++ src/transaction/transactionService.js
@@ -1,14 +1,53 @@
 import { TransactionModel } from './transaction.js';
+import UserModel from '../user/user.js';
 
 export const createTransaction = async (req, res) => {
   try {
-    const newTransaction = new TransactionModel(req.body);
+    const userData = await UserModel.findOne({ email: req.user.email })
+      .populate('assets')
+      .populate('lastestTransactions');
+    console.log({ userData });
+    const { lastestTransactions, id: userId } = userData;
+    const { description, asset, amount } = req.body;
+    // finding previous transaction which belong to the same asset type
+    const prevTrans = lastestTransactions
+      ? lastestTransactions.find((item) => `${item.asset}` === `${asset}`)
+      : null;
+    console.log({ prevTrans });
+    let payload = {
+      description,
+      asset,
+      amount,
+      user: userId,
+    };
+    if (prevTrans) {
+      const { id: prevId, total } = prevTrans;
+      payload = {
+        ...payload,
+        prev: prevId,
+        total: total + amount,
+      };
+    } else {
+      payload = {
+        ...payload,
+        total: amount,
+      };
+    }
+    console.log({ payload });
+    const newTransaction = new TransactionModel(payload);
     const saved = await newTransaction.save();
-    const transaction = await TransactionModel.findById(saved._id)
+    const transaction = await TransactionModel.findById(saved.id)
       .populate('user')
       .populate('asset')
       .populate('prev');
-
+    if (prevTrans) {
+      userData.lastestTransactions.pull(prevTrans.id);
+      userData.lastestTransactions.push(saved.id);
+      await userData.save();
+    } else {
+      userData.lastestTransactions.push(saved.id);
+      await userData.save();
+    }
     return res.status(200).json(transaction.toJSON({ virtuals: true }));
   } catch (error) {
     return res.status(400).json(error);
@@ -16,7 +55,6 @@ export const createTransaction = async (req, res) => {
 };
 export const getTransaction = async (req, res) => {
   const { user, asset } = req.query;
-  console.log({ user, asset });
   try {
     const transactions = await TransactionModel.find({ user, asset })
       .populate('asset')
